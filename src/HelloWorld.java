605992874e75a25eeb90bd3175f26e9868c95781
Pooja Salpekar <pooja@poojasalp.(none)>
2012-09-25 14:46:26 +0530

    build script for junit test

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pooja
+ * Date: 24/09/12
+ * Time: 10:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.print("Hello World");
+    }
+}
---
+++ test/HelloWorldTest.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pooja
+ * Date: 25/09/12
+ * Time: 12:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorldTest {
+    @Test
+    public void testThatAlwaysPass(){
+        Assert.assertTrue(true);
+    }
+}
